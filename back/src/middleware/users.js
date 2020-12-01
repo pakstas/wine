a3e2b377e92ee7ea6301d69bc1afe8641c46f8c3
Egidijus <egidijus@Egidijuss-MacBook-Air.local>
2020-12-01 23:51:31 +0200

    change username to email

--- back/src/middleware/users.js
+++ back/src/middleware/users.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 module.exports = {
   validateRegistration: (req, res, next) => {
-    if (!req.body.username || req.body.username.length < 6) {
+    if (!req.body.email || req.body.email.length < 8) {
       return res.status(400).json({
-        msg: "Username does not follow the rules",
+        msg: "User email does not follow the rules",
       });
     }
     if (!req.body.password || req.body.password.length < 8) {
--- back/src/router.js
+++ back/src/router.js
@@ -14,71 +14,59 @@ router.get("/hidden", middleware.isLoggedIn, (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  const username = req.body.username.toLowerCase();
-  con.query(
-    `SELECT * FROM users WHERE username = '${username}'`,
-    (err, result) => {
-      if (err || result.length === 0) {
-        err
-          ? res.status(400).json({ msg: "Problem with DB." })
-          : res.status(400).json({ msg: "No such user." });
-      } else {
-        bcrypt.compare(
-          req.body.password,
-          result[0].password,
-          (bErr, bResult) => {
-            if (bErr || !bResult) {
-              res
-                .status(400)
-                .json({ msg: "The username password is not correct" });
-            } else {
-              if (bResult) {
-                const token = jwt.sign(
-                  { userId: result[0].id, username: result[0].username },
-                  "SECRETKEY",
-                  { expiresIn: "7d" }
-                );
-                res.status(200).json({ msg: "Logged In", token });
-              }
-            }
+  const email = req.body.email.toLowerCase();
+  con.query(`SELECT * FROM w_users WHERE email = '${email}'`, (err, result) => {
+    if (err || result.length === 0) {
+      err
+        ? res.status(400).json({ msg: "Problem with DB." })
+        : res.status(400).json({ msg: "No such user." });
+    } else {
+      bcrypt.compare(req.body.password, result[0].password, (bErr, bResult) => {
+        if (bErr || !bResult) {
+          res.status(400).json({ msg: "The username password is not correct" });
+        } else {
+          if (bResult) {
+            const token = jwt.sign(
+              { userId: result[0].id, email: result[0].email },
+              "SECRETKEY",
+              { expiresIn: "7d" }
+            );
+            res.status(200).json({ msg: "Logged In", token });
           }
-        );
-      }
+        }
+      });
     }
-  );
+  });
 });
 
 router.post("/register", middleware.validateRegistration, (req, res) => {
-  const username = req.body.username.toLowerCase();
-  con.query(
-    `SELECT * FROM users WHERE username = '${username}'`,
-    (err, result) => {
-      if (err) {
-        res.status(400).json({ msg: "The DB is broken." });
-      } else if (result.length !== 0) {
-        res.status(400).json({ msg: "The user already exists" });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            res.status(400).json(err);
-          } else {
-            con.query(
-              `INSERT INTO users (username, password, registration_date) VALUES ('${username}', '${hash}', now())`,
-              (err, result) => {
-                if (err) {
-                  res.status(400).json(err);
-                } else {
-                  res
-                    .status(201)
-                    .json({ msg: "USER has registered succesfully." });
-                }
+  const email = req.body.email.toLowerCase();
+  con.query(`SELECT * FROM w_users WHERE email = '${email}'`, (err, result) => {
+    if (err) {
+      res.status(400).json({ msg: "The DB is broken." });
+    } else if (result.length !== 0) {
+      res.status(400).json({ msg: "The user email already exists" });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          con.query(
+            `INSERT INTO w_users (email, password) VALUES ('${email}', '${hash}'`,
+            (err, result) => {
+              if (err) {
+                res.status(400).json(err);
+              } else {
+                res
+                  .status(201)
+                  .json({ msg: "USER email has registered succesfully." });
               }
-            );
-          }
-        });
-      }
+            }
+          );
+        }
+      });
     }
-  );
+  });
 });
 
 module.exports = router;

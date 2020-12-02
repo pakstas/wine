1c87529a5973170662886136e1f75a97a1feb017
Egidijus <egidijus@Egidijuss-MacBook-Air.local>
2020-12-02 20:03:32 +0200

    added WineType post and get

--- back/src/router.js
+++ back/src/router.js
@@ -13,30 +13,83 @@ router.get("/hidden", middleware.isLoggedIn, (req, res) => {
   res.send("This is hidden information");
 });
 
-router.post("/login", (req, res) => {
-  const email = req.body.email.toLowerCase();
-  con.query(`SELECT * FROM w_users WHERE email = '${email}'`, (err, result) => {
+router.get("/winetype", middleware.isLoggedIn, (req, res) => {
+  con.query(`SELECT * FROM wine_types`, (err, result) => {
     if (err || result.length === 0) {
       err
         ? res.status(400).json({ msg: "Problem with DB." })
-        : res.status(400).json({ msg: "No such user." });
+        : res.status(400).json({ msg: "No wine type records found." });
     } else {
-      bcrypt.compare(req.body.password, result[0].password, (bErr, bResult) => {
-        if (bErr || !bResult) {
-          res.status(400).json({ msg: "The username password is not correct" });
-        } else {
-          if (bResult) {
-            const token = jwt.sign(
-              { userId: result[0].id, email: result[0].email },
-              "SECRETKEY",
-              { expiresIn: "7d" }
-            );
-            res.status(200).json({ msg: "Logged In", token });
-          }
-        }
-      });
+      res.json(result);
     }
   });
+});
+
+router.post("/login", (req, res) => {
+  if (req.body.email && req.body.password) {
+    const email = req.body.email.toLowerCase();
+    con.query(
+      `SELECT * FROM w_users WHERE email = '${email}'`,
+      (err, result) => {
+        if (err || result.length === 0) {
+          err
+            ? res.status(400).json({ msg: "Problem with DB." })
+            : res.status(400).json({ msg: "No such user." });
+        } else {
+          bcrypt.compare(
+            req.body.password,
+            result[0].password,
+            (bErr, bResult) => {
+              if (bErr || !bResult) {
+                res
+                  .status(400)
+                  .json({ msg: "The username password is not correct" });
+              } else {
+                if (bResult) {
+                  const token = jwt.sign(
+                    { userId: result[0].id, email: result[0].email },
+                    "SECRETKEY",
+                    { expiresIn: "7d" }
+                  );
+                  res.status(200).json({ msg: "Logged In", token });
+                }
+              }
+            }
+          );
+        }
+      }
+    );
+  } else {
+    res.status(400).json({ msg: "User email or password not entered." });
+  }
+});
+
+router.post("/addtype", middleware.isLoggedIn, (req, res) => {
+  console.log(req.body);
+  if (
+    req.body.name !== "" &&
+    req.body.reqion !== "" &&
+    req.body.type !== "" &&
+    req.body.year !== ""
+  ) {
+    // TODO: check if there is already a wine that is wanted to submit
+    con.query(
+      `INSERT INTO wine_types (name, region, type, year) VALUES('${req.body.name}', '${req.body.region}', '${req.body.type}', ${req.body.year})`,
+      (err, result) => {
+        if (err) {
+          res.status(400).json({
+            msg: "We cannot add your wine to DB. Please try again later.",
+          });
+        } else {
+          res.status(200).json({ msg: "Your wine type added to DB." });
+        }
+      }
+    );
+  } else {
+    res
+      .status(400)
+      .json({ msg: "Check if you have filled all required fields." });
+  }
 });
 
 router.post("/register", middleware.validateRegistration, (req, res) => {
